fa5cff68a83f958bbabe4f3abf705b3112c7c00b
Angel <47459464+vihangel@users.noreply.github.com>
2022-09-19 18:27:17 -0300

    fix audio controller

--- lib/app/modules/story/story_page.dart
+++ lib/app/modules/story/story_page.dart
@@ -47,164 +47,169 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     ScrollController scrollController = ScrollController();
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(4.0),
-          child: Container(
-            color: ColorsApp.gray,
-            height: 1.0,
-            width: size.width * 0.9,
+    return Observer(builder: (context) {
+      players.onDurationChanged.listen((Duration duration) {
+        controller.duration = duration;
+      });
+      return Scaffold(
+        appBar: AppBar(
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(4.0),
+            child: Container(
+              color: ColorsApp.gray,
+              height: 1.0,
+              width: size.width * 0.9,
+            ),
           ),
-        ),
-        foregroundColor: ColorsApp.white,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () {
-            Modular.to.pop();
-          },
-          icon: const Icon(Icons.close),
-        ),
-        actions: [
-          Observer(builder: (context) {
-            controller.getPosition(players);
+          foregroundColor: ColorsApp.white,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {
+              Modular.to.pop();
+            },
+            icon: const Icon(Icons.close),
+          ),
+          actions: [
+            Observer(builder: (context) {
+              controller.getPosition(players);
 
-            return controller.duration != null &&
-                    controller.duration != Duration.zero
-                ? Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(controller.label(),
-                            style: TextStyles.subTitleCard),
-                        SizedBox(
-                          width: 150.w,
-                          child: Slider(
-                            value: double.parse(
-                                controller.positionSlider.toString()),
-                            min: 0,
-                            max: double.parse(
-                                controller.duration!.inSeconds.toString()),
-                            divisions: int.parse(
-                                controller.duration!.inSeconds.toString()),
-                            //  duration!.inSeconds,
-                            label: controller.label(),
-                            onChangeEnd: (value) {
-                              controller.onChangeSlider(value, players);
-                            },
-                            onChanged: (value) {
-                              controller.onChangeSlider(value, players);
-                            },
-                          ),
-                        ),
-                        !controller.isPaused
-                            ? IconButton(
-                                onPressed: () {
-                                  players.pause();
-                                  controller.isPaused = true;
-                                },
-                                icon: const Icon(Icons.pause),
-                              )
-                            : IconButton(
-                                onPressed: () async {
-                                  players.resume();
-
-                                  controller.isPaused = false;
-                                },
-                                icon: const Icon(Icons.play_arrow_rounded)),
-                        TextButton(
-                          onPressed: () {
-                            controller.setPlayback(players);
-                            controller.isPaused = true;
-                          },
-                          child: Text("${controller.playBack}x",
+              return controller.duration != null &&
+                      controller.duration != Duration.zero
+                  ? Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(controller.label(),
                               style: TextStyles.subTitleCard),
-                        ),
-                      ],
-                    ),
-                  )
-                : Container();
-          }),
-        ],
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: SizedBox(
-          height: size.height,
-          child: ListView(
-            controller: scrollController,
-            physics: const BouncingScrollPhysics(),
-            shrinkWrap: true,
-            children: [
-              SizedBox(
-                height: 20.h,
-              ),
-              SizedBox(
-                height: 150.h,
-                child: CardWidget(
-                  icon: widget.story.icon ?? 'kris.png',
-                  subTitle: widget.story.subTitle ?? '',
-                  title: widget.story.title ?? '',
+                          SizedBox(
+                            width: 150.w,
+                            child: Slider(
+                              value: double.parse(
+                                  controller.positionSlider.toString()),
+                              min: 0,
+                              max: double.parse(
+                                  controller.duration!.inSeconds.toString()),
+                              divisions: int.parse(
+                                  controller.duration!.inSeconds.toString()),
+                              //  duration!.inSeconds,
+                              label: controller.label(),
+                              onChangeEnd: (value) {
+                                controller.onChangeSlider(value, players);
+                              },
+                              onChanged: (value) {
+                                controller.onChangeSlider(value, players);
+                              },
+                            ),
+                          ),
+                          !controller.isPaused
+                              ? IconButton(
+                                  onPressed: () {
+                                    players.pause();
+                                    controller.isPaused = true;
+                                  },
+                                  icon: const Icon(Icons.pause),
+                                )
+                              : IconButton(
+                                  onPressed: () async {
+                                    players.resume();
+
+                                    controller.isPaused = false;
+                                  },
+                                  icon: const Icon(Icons.play_arrow_rounded)),
+                          TextButton(
+                            onPressed: () {
+                              controller.setPlayback(players);
+                              controller.isPaused = true;
+                            },
+                            child: Text("${controller.playBack}x",
+                                style: TextStyles.subTitleCard),
+                          ),
+                        ],
+                      ),
+                    )
+                  : Container();
+            }),
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: SizedBox(
+            height: size.height,
+            child: ListView(
+              controller: scrollController,
+              physics: const BouncingScrollPhysics(),
+              shrinkWrap: true,
+              children: [
+                SizedBox(
+                  height: 20.h,
                 ),
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  widget.story.information!.voice != null
-                      ? IconButton(
-                          icon: const Icon(Icons.volume_up,
-                              color: ColorsApp.white),
-                          onPressed: () async {
-                            await players.play(
-                              AssetSource(
-                                  'audio/${widget.story.information!.voice}'),
-                            );
-
-                            controller.isPaused = false;
-
-                            controller.duration = await players.getDuration();
-
-                            print(
-                                'Duration: ${controller.duration!.inSeconds}');
-                          },
-                        )
-                      : Container(),
-                  Text(
-                    widget.story.title ?? '',
-                    style: TextStyles.title,
-                    textAlign: TextAlign.center,
+                SizedBox(
+                  height: 150.h,
+                  child: CardWidget(
+                    icon: widget.story.icon ?? 'kris.png',
+                    subTitle: widget.story.subTitle ?? '',
+                    title: widget.story.title ?? '',
                   ),
-                ],
-              ),
-              Text(
-                widget.story.information!.text!,
-                style: TextStyles.regular,
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-              Container(
-                width: size.width,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.r),
                 ),
-                child: Image.asset(
-                  'assets/img/${widget.story.information!.img![0]}',
-                  fit: BoxFit.cover,
+                SizedBox(
+                  height: 20.h,
                 ),
-              ),
-              SizedBox(
-                height: 60.h,
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    widget.story.information!.voice != null
+                        ? IconButton(
+                            icon: const Icon(Icons.volume_up,
+                                color: ColorsApp.white),
+                            onPressed: () async {
+                              await players.play(
+                                AssetSource(
+                                    'audio/${widget.story.information!.voice}'),
+                              );
+
+                              controller.isPaused = false;
+
+                              print(
+                                  'Duration: ${controller.duration?.inSeconds}');
+                            },
+                          )
+                        : Container(),
+                    Text(
+                      widget.story.title ?? '',
+                      style: TextStyles.title,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+                Text(
+                  widget.story.information!.text!,
+                  style: TextStyles.regular,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                widget.story.information!.img != null
+                    ? Container(
+                        width: size.width,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30.r),
+                        ),
+                        child: Image.asset(
+                          'assets/img/${widget.story.information!.img![0]}',
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    : Container(),
+                SizedBox(
+                  height: 60.h,
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -8,7 +8,6 @@
 
 import 'package:audioplayers_web/audioplayers_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
-import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -16,6 +15,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   AudioplayersPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
-  UrlLauncherPlugin.registerWith(registrar);
+
   registrar.registerMessageHandler();
 }
